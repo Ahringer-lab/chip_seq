ed84327ced6e9b114cce5e440d8275706cf43963
Stewalsh04 <SW2154@cam.ac.uk>
2024-08-07 13:06:08 +0100

    Added header

--- bw.bash
+++ bw.bash
@@ -7,6 +7,21 @@
 #SBATCH --mem=50gb
 #SBATCH --output=pipeline_%j.log # Standard output and error log
 
+##########################################################################################################################################################################
+############################## BigWig sbatch script froma sample sheet #############################################################################
+# This code will gather all the bam file names listed in the bam sample sheet from the input folder into an array to initiate pipeline runs for each one in the HPC
+# The script will make a bw file for each bam file found in the sample sheet
+# The script creates a parent directory with the run ID, this is a date/time stamp unless specified as an option
+# The script will create a folder for each bam file with the bam id as it's name within the parent folder
+# Remember to change the SBATCH options above to configure for your run
+# This script should only be run on the HPC using sbatch
+# Options include:
+#      threads = Will multi-thread any process to this number - Not currently used
+#      input = Change the path of the input fastq files, default is ~/data
+#      id = Change the name of the parent pipeline output folder, the default is a datestamp
+# Author Steve Walsh Aug 2024
+###########################################################################################################################################################################
+
 #Uncomment below for debugging
 #set -x
 
